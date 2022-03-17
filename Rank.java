5a9bef5c8a849afcfe994d36688e89135b7ff27e
AbhinavSyalYORK <98564152+AbhinavSyalYORK@users.noreply.github.com>
2022-03-17 17:22:34 -0400

    Add files via upload

---
+++ Rank.java
@@ -0,0 +1,76 @@
+package com.example.leaderboard;
+
+public class Rank
+{
+    private String playerName;
+    private boolean isSpeed;
+    private double time;
+    private int score;
+    private String difficulty;
+
+    public Rank (String playerName, int score, String difficulty)
+    {
+        this.playerName = playerName;
+        this.score = score;
+        this.difficulty = difficulty;
+    }
+
+    public Rank (String playerName, boolean isSpeed, double time, int score, String difficulty)
+    {
+        this.playerName = playerName;
+        this.isSpeed = isSpeed;
+        this.time = time;
+        this.score = score;
+        this.difficulty = difficulty;
+    }
+
+    public void setPlayerName (String playerName)
+    {
+        this.playerName = playerName;
+    }
+
+    public void setIsSpeed (boolean isSpeed)
+    {
+        this.isSpeed = isSpeed;
+    }
+
+    public void setTime (double time)
+    {
+        this.time = time;
+    }
+
+    public void setScore (int score)
+    {
+        this.score = score;
+    }
+
+    public void setDifficulty (String difficulty)
+    {
+        this.difficulty = difficulty;
+    }
+
+    public String getPlayerName()
+    {
+        return playerName;
+    }
+
+    public Boolean getIsSpeed()
+    {
+        return isSpeed;
+    }
+    public double getTime()
+    {
+        return time;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public String getDifficulty()
+    {
+        return difficulty;
+    }
+
+}
