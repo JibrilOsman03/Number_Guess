eb0505dca158b135b4dc329d8ab569231e1b7fb9
JibrilOsman03 <98564145+JibrilOsman03@users.noreply.github.com>
2022-04-01 16:42:30 -0400

    Add files via upload

---
+++ SettingActivity.java
@@ -0,0 +1,17 @@
+package com.example.number_guess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//import com.example.number_guess;
+
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_screen);
+    }
+}
